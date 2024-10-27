b39586bd6b7436b0c97e25304b89738dcf1ffbb7
root <root@LAPTOP-G7SDFTEL>
2024-10-27 15:07:56 +0800

    test ch4

--- os/src/mm/frame_allocator.rs
+++ os/src/mm/frame_allocator.rs
@@ -1,7 +1,7 @@
 //! Implementation of [`FrameAllocator`] which
 //! controls all the frames in the operating system.
 
-use super::{PhysAddr, PhysPageNum};
+use super::{PhysAddr, PhysPageNum, VirtPageNum};
 use crate::config::MEMORY_END;
 use crate::sync::UPSafeCell;
 use alloc::vec::Vec;
@@ -55,6 +55,9 @@ impl StackFrameAllocator {
         self.current = l.0;
         self.end = r.0;
         // trace!("last {} Physical Frames.", self.end - self.current);
+    }
+    pub fn is_enough(&self, vpn_start: VirtPageNum, vpn_end: VirtPageNum) -> bool {
+        return vpn_end.0 - vpn_start.0 < self.recycled.len() + self.end - self.current;
     }
 }
 impl FrameAllocator for StackFrameAllocator {
